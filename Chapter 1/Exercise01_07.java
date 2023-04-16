2ae86815e6d903bf291c2ee0478c4f010499cc9c
Kazi Sean <keh8423@nyu.edu>
2023-04-16 18:32:18 -0400

    updated chapter 1

---
+++ Chapter 1/Exercise01_07.java
@@ -0,0 +1,20 @@
+/*
+ *          (Approximatep) p can be computed using the following formula:
+ * 
+ */
+
+public class Exercise01_07 {
+    public static void main (String[] args){
+
+        // first part
+        System.out.println(
+            4 * ( 1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0)  -  (1.0/11.0))
+        );
+
+        // second part
+        System.out.println(
+            4 * ( 1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0)  -  (1.0/11.0) + (1.0 / 13.0))
+        );
+        
+    }
+}
---
+++ Chapter 1/Exercise01_08.java
@@ -0,0 +1,12 @@
+/*
+ * (Area and perimeter of a circle) Write a program that displays the area and perimeter of a circle that has a radius of 5.5 using the formula
+ */
+
+
+ public class Exercise01_08 {
+    public static void main (String [] args){
+        System.out.println("Area = " + (5.5 * 5.5) * 3.14159);
+        System.out.println("Perimeter = " +2.0 * 5.5 * 3.14159);
+
+    }
+}
