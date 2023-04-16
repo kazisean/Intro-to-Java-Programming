3e85f40576390c7615be34240cd7b82e2afb2742
Kazi Sean <keh8423@nyu.edu>
2023-04-16 17:48:58 -0400

    new

---
+++ Chapter 1/Exercise01_01.java
@@ -0,0 +1,14 @@
+/*
+ *          (Display three messages) Write a program that displays Welcome to Java,
+*           Welcome to Computer Science, and Programming is fun.
+ * 
+ */
+
+
+public class Exercise01_01 {
+    public static void main( String [] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.print("Programming is fun");
+    }
+}
