10175564f039253afa62eed5bb6fa7fd4866cc50
Kazi Sean <kazisean@naver.com>
2023-04-17 12:00:27 -0400

    updated chapter 1

---
+++ Chapter 1/Exercise01_09.java
@@ -0,0 +1,11 @@
+/*
+*           Description : (Area and perimeter of a rectangle) Write a program that displays the area and perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following formula:
+*                           Area = width * height
+*/
+
+public class Exercise01_09 {
+    public static void main (String [] args){
+        System.out.println("Area = " + 4.5 * 7.9);
+        System.out.println("Perimeter = " + 2.0 * (4.5 + 7.9));
+    }
+}
---
+++ Chapter 1/Exercise01_10.java
@@ -0,0 +1,16 @@
+/*
+ *              Description : (Average speed in miles) Assume a runner runs 14 kilometers in 45 minutes and 30 seconds. Write a program that displays the average speed in miles per hour. (Note that 1 mile is 1.6 kilometers.)
+ * 
+ */
+
+public class Exercise01_10 {
+    public static void main (String [] args){
+
+
+        System.out.println(
+
+            "The average speed in miles per hour is " + (14/1.6) / 45.5 * 60
+
+        );
+    }
+}
