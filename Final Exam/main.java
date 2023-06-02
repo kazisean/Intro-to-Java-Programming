41881769afb327da7c53018ac2291613a5f42489
Kazi Sean <keh8423@nyu.edu>
2023-06-01 23:32:28 -0400

    save

---
+++ Final Exam/main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    
+
+    public static void main (String[] args){
+
+        List L = new ArrayList();
+
+
+        System.out.println(L.size());
+
+        int [] array = {1,2,3,4};
+
+        for (int i : array){
+            System.out.println(i);
+        }
+
+    }
+
+}
