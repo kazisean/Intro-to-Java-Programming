712503c0fcfd073f961a89b11e90aca4f9482d5b
Kazi Hossain <keh8423@FAS.NYU>
2023-06-29 16:11:27 -0400

    .

---
+++ Chapter 2/Exercise02_07.java
@@ -0,0 +1,27 @@
+/*
+ * 
+ * (Find the number of years) Write a program that prompts the user to enter the
+ * 
+ * minutes (e.g., 1 billion), and displays the number of years and days for the
+ * minutes. For simplicity, assume a year has 365 days.
+ * 
+ */
+
+import java.util.*;
+
+public class Exercise02_07 {
+    public static void main(Srting[] args) {
+	// user inputs
+	String usrNum;
+
+	// asking user for the number 
+	System.out.println("Enter the number of minutes: ");
+	usrNum = nextLine();
+	
+	// calculations
+	int oneDay = 1440;
+	
+
+	
+    }
+}
